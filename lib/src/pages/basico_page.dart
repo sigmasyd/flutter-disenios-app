3ffdc31ccf79f0d3880556dff22daf39f5fddb96
Carlos Barcos <fbarcos8@hotmail.com>
2021-03-21 12:17:48 -0500

    Se finaliza el diseño basico. 	modificado:     lib/src/pages/basico_page.dart

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
@@ -9,52 +11,66 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        _crearImagen(),
-        _crearTitulo(),
-        _crearAcciones(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-      ],
-    ));
+      // Column: el comportamiento por defecto es que no es scrollable
+      // Listview funciona pero queda el detalle del notch(la barra) no se ve bien
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ]
+        ),
+      )
+    );
   }
 
   Widget _crearImagen() {
-    return Image(image: NetworkImage(this.imageUrl));
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(this.imageUrl),
+        height: 250.0,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: [
-          Expanded(
-            // ocupa el espacio restante
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Volcan nevado', style: estiloTitulo),
-                SizedBox(height: 7.0),
-                Text('Volcan cubierto de nieve', style: estiloSubtitulo),
-              ],
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: [
+            Expanded(
+              // ocupa el espacio restante
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Volcan nevado', style: estiloTitulo),
+                  SizedBox(height: 7.0),
+                  Text('Volcan cubierto de nieve', style: estiloSubtitulo),
+                ],
+              ),
             ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red,
-            size: 30.0,
-          ),
-          Text(
-            '41',
-            style: TextStyle(fontSize: 20.0),
-          )
-        ],
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text(
+              '41',
+              style: TextStyle(fontSize: 20.0),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -83,13 +99,14 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto() {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 40.0,
-        vertical: 20.0,
+    return SafeArea(
+          child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 40.0,
+        ),
+        child: Text('Excepteur sit officia mollit dolore enim reprehenderit. Anim occaecat consequat ipsum laborum. Esse deserunt ad non officia veniam deserunt veniam eu ipsum. Eiusmod aliquip sit magna adipisicing mollit ex excepteur incididunt consectetur fugiat nisi dolor ea. Cupidatat id excepteur ipsum reprehenderit est dolor eiusmod in eiusmod consequat duis.',
+        textAlign: TextAlign.justify,)
       ),
-      child: Text('Excepteur sit officia mollit dolore enim reprehenderit. Anim occaecat consequat ipsum laborum. Esse deserunt ad non officia veniam deserunt veniam eu ipsum. Eiusmod aliquip sit magna adipisicing mollit ex excepteur incididunt consectetur fugiat nisi dolor ea. Cupidatat id excepteur ipsum reprehenderit est dolor eiusmod in eiusmod consequat duis.',
-      textAlign: TextAlign.justify,)
     );
   }
 }
