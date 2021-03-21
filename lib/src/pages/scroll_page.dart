a28b8479c78955d03263af2022c0a252ba575bb2
Carlos Barcos <fbarcos8@hotmail.com>
2021-03-21 12:46:28 -0500

    Terminando la primera pagina: 1/2 	modificado:     lib/src/pages/scroll_page.dart

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -12,14 +12,52 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: [
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
 
   Widget _pagina2() {
     return Center(
       child: Text('Pagina 2'),
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/landing.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.0),
+          Text('11 grados',style: estiloTexto),
+          Text('Miercoles',style: estiloTexto),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down,size: 70.0,color: Colors.white)
+        ],
+      ),
     );
   }
 }
