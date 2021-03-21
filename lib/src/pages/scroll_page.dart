dc7928dda7145a05da5b3a8557150070e651fc97
Carlos Barcos <fbarcos8@hotmail.com>
2021-03-21 14:15:37 -0500

    Finalizando segundo ejemplo
    	modificado:     lib/src/pages/scroll_page.dart

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -21,12 +21,6 @@ class ScrollPage extends StatelessWidget {
     );
   }
 
-  Widget _pagina2() {
-    return Center(
-      child: Text('Pagina 2'),
-    );
-  }
-
   Widget _colorFondo() {
     return Container(
       width: double.infinity,
@@ -52,11 +46,30 @@ class ScrollPage extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 20.0),
-          Text('11 grados',style: estiloTexto),
-          Text('Miercoles',style: estiloTexto),
+          Text('11 grados', style: estiloTexto),
+          Text('Miercoles', style: estiloTexto),
           Expanded(child: Container()),
-          Icon(Icons.keyboard_arrow_down,size: 70.0,color: Colors.white)
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
         ],
+      ),
+    );
+  }
+
+  Widget _pagina2() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+      child: Center(
+        child: ElevatedButton(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 20.0),
+            child: Text('Bienvenidos',style: TextStyle(fontSize: 25.0))
+          ),
+          onPressed: () {
+
+          },
+        ),
       ),
     );
   }
